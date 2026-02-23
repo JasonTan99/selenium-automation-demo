78db8dbc56c818aecdc9b4604ebf5351cc0ad1f9
JasonTan99 <jasontanjsheng.15@gmail.com>
2026-02-23 15:27:15 +0800

    clone

--- test_Selenium.py
+++ test_Selenium.py
@@ -38,5 +38,5 @@ def test_login(driver):
         EC.presence_of_element_located((By.XPATH, "//span[@class='title' and @data-test='title']")))
         print("Login successful")
     except TimeoutException:
-        print("Login failed or page did not load.") #comment
+        print("Login failed or page did not load.") #commenttt
     time.sleep(20)
